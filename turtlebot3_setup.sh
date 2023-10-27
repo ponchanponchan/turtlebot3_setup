e12c6bbacc650afeb98897734a66e021ef5a63ba
ponchanponchan <147683303+ponchanponchan@users.noreply.github.com>
2023-10-27 11:55:30 +0900

    Update turtlebot3_setup.sh

--- turtlebot3_setup.sh
+++ turtlebot3_setup.sh
@@ -1,23 +1,23 @@
 ## setting dependencies for TurtleBot3
 
 # Install Gazebo11
-sudo apt-get install ros-foxy-gazebo-* -y
-sudo apt install ros-foxy-cartographer -y
-sudo apt install ros-foxy-cartographer-ros -y
+sudo apt-get install ros-noetic-gazebo-* -y
+sudo apt install ros-noetic-cartographer -y
+sudo apt install ros-noetic-cartographer-ros -y
 
 # Install Cartographer
-sudo apt install ros-foxy-navigation2 -y
-sudo apt install ros-foxy-cartographer-ros -y
+sudo apt install ros-noetic-navigation2 -y
+sudo apt install ros-noetic-cartographer-ros -y
 
 # Install Navigation2
-sudo apt install ros-foxy-navigation2 -y
-sudo apt install ros-foxy-nav2-bringup -y
+sudo apt install ros-noetic-navigation2 -y
+sudo apt install ros-noetic-nav2-bringup -y
 
 # Install TurtleBot3 Packages
 source ~/.bashrc
-sudo apt install ros-foxy-dynamixel-sdk -y
-sudo apt install ros-foxy-turtlebot3-msgs -y
-sudo apt install ros-foxy-turtlebot3 -y
+sudo apt install ros-noetic-dynamixel-sdk -y
+sudo apt install ros-noetic-turtlebot3-msgs -y
+sudo apt install ros-noetic-turtlebot3 -y
 
 # Environment Configuration
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
